dd86a00043b6c423fd4f3330802a6d25e2842c17
Daniel Koska <danielkoska86@gmail.com>
2023-06-05 14:10:19 +0200

    Cleaned up existing and added further test functions

--- tests/testthat/test-FunBootBand.R
+++ tests/testthat/test-FunBootBand.R
@@ -1,41 +1,32 @@
-library(testthat)
 library(FunBootBand)
-# devtools::load_all()
 
-# 1. Test the function behavior with valid inputs
-test_that("band() returns something", {
-  tmp <- band(data,
-              type = "prediction",
-              alpha = 0.05,
-              iid = FALSE,
-              k.coef = 50,
-              B = 5)
-  expect_type(tmp, "double")
+# 0. Test if the function runs without error (assuming valid inputs)
+expect_no_error(band(data,
+                     type = "prediction",
+                     alpha = 0.05,
+                     iid = FALSE,
+                     k.coef = 50,
+                     B = 5))
+
+# 1. Does the function output have a valid data type?
+test_that("band() has a valid return type", {
+  expect_type(object = band(data,
+                            type = "prediction",
+                            alpha = 0.05,
+                            iid = FALSE,
+                            k.coef = 50,
+                            B = 5),
+              type = "double")
 })
 
-# 2. Does the code return the expected value?
-load("~/FunBootBand/data/curvesample.RData")
-
-# iid case
-dat <- data[-1, ]
-
-test_that("band() returns something", {
-  tmp <- band(dat,
-              type = "prediction",
-              alpha = 0.05,
-              iid = TRUE,
-              k.coef = 50,
-              B = 5)
-  expect_type(tmp, "double")
-})
-
-# Assume iid when data is not iid or
-# ...
-
-# Check for NAs
-# Currently, NAs are not allowed.
-
-# Wrong data input format (iid)
+# 2. Does the function output have the correct length?
+expect_length(object = band(data,
+                            type = "prediction",
+                            alpha = 0.05,
+                            iid = FALSE,
+                            k.coef = 50,
+                            B = 5),
+              n = 3*dim(data)[1]) # n = expected length
 
 # 3. Test the function behavior with invalid inputs
 test_that("Invalid inputs throw appropriate errors", {
@@ -53,8 +44,50 @@ test_that("Invalid inputs throw appropriate errors", {
                "'alpha' must be a numeric value between 0 and 1.")
 })
 
+# 4. Test if the header is specified correctly
+data.without.names <- data.frame(matrix(unlist(data), nrow = ncol(data)))
+test_that("Invalid header", {
+  expect_error(band(data.without.names, type = "prediction", alpha = 0.05, iid = FALSE, k.coef = 50, B = 5),
+               "The header does not\n
+        indicate a nested structure even though 'iid' is set to 'FALSE'.")
+})
+
+
+# Further 'expectations': https://testthat.r-lib.org/reference/
+# expect_named() # Does code return a vector with (given) names?
+# expect_true() expect_false() Does code return TRUE or FALSE?
+
+# What about NAs?
+
+# # Weiterer Test
+# load("~/FunBootBand/data/curvesample.RData")
+#
+# # iid case
+# dat <- data[-1, ]
+#
+# test_that("band() returns something", {
+#   tmp <- band(dat,
+#               type = "prediction",
+#               alpha = 0.05,
+#               iid = TRUE,
+#               k.coef = 50,
+#               B = 5)
+#   expect_type(tmp, "double")
+# })
+
+# Assume iid when data is not iid or
+# ...
+
+# Check for NAs
+# Currently, NAs are not allowed.
+
+# Wrong data input format (iid)
+
+
+
+
+
 # 4. Data Structure Test
-# expect_length(): Checks the length of an object.
 
 # Equality Tests:
 #   expect_equal(): Checks if two objects are equal.
