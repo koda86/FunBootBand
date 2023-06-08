f5c6053ad403c76564e1e32567052c4bf78406fe
Daniel Koska <danielkoska86@gmail.com>
2023-06-08 09:27:32 +0200

    Implemented check for handling of NA's

--- tests/testthat/test-FunBootBand.R
+++ tests/testthat/test-FunBootBand.R
@@ -1,6 +1,6 @@
 library(FunBootBand)
 
-# 0. Test if the function runs without error (assuming valid inputs)
+# Test if the function runs without error (assuming valid inputs)
 expect_no_error(band(data,
                      type = "prediction",
                      alpha = 0.05,
@@ -8,7 +8,7 @@ expect_no_error(band(data,
                      k.coef = 50,
                      B = 5))
 
-# 1. Does the function output have a valid data type?
+# Does the function output have a valid data type?
 test_that("band() has a valid return type", {
   expect_type(object = band(data,
                             type = "prediction",
@@ -19,7 +19,7 @@ test_that("band() has a valid return type", {
               type = "double")
 })
 
-# 2. Does the function output have the correct length?
+# Does the function output have the correct length?
 expect_length(object = band(data,
                             type = "prediction",
                             alpha = 0.05,
@@ -28,7 +28,7 @@ expect_length(object = band(data,
                             B = 5),
               n = 3*dim(data)[1]) # n = expected length
 
-# 3. Test the function behavior with invalid inputs
+# Test the function behavior with invalid inputs
 test_that("Invalid inputs throw appropriate errors", {
   # Invalid 'type' (a)
   expect_error(band(matrix(1:10), type = "invalid", alpha = 0.05, iid = TRUE, k.coef = 50, B = 5),
@@ -44,7 +44,7 @@ test_that("Invalid inputs throw appropriate errors", {
                "'alpha' must be a numeric value between 0 and 1.")
 })
 
-# 4. Test if the header is specified correctly
+# Test if the header is specified correctly
 data.without.names <- data.frame(matrix(unlist(data), nrow = ncol(data)))
 test_that("Invalid header", {
   expect_error(band(data.without.names, type = "prediction", alpha = 0.05, iid = FALSE, k.coef = 50, B = 5),
@@ -52,12 +52,20 @@ test_that("Invalid header", {
         indicate a nested structure even though 'iid' is set to 'FALSE'.")
 })
 
+# Test handling of NA's
+data.na <- data
+data.na[1,1] <- NA
+expect_error(band(data.na,
+                     type = "prediction",
+                     alpha = 0.05,
+                     iid = FALSE,
+                     k.coef = 50,
+                     B = 5))
 
 # Further 'expectations': https://testthat.r-lib.org/reference/
 # expect_named() # Does code return a vector with (given) names?
 # expect_true() expect_false() Does code return TRUE or FALSE?
 
-# What about NAs?
 
 # # Weiterer Test
 # load("~/FunBootBand/data/curvesample.RData")
@@ -78,8 +86,6 @@ test_that("Invalid header", {
 # Assume iid when data is not iid or
 # ...
 
-# Check for NAs
-# Currently, NAs are not allowed.
 
 # Wrong data input format (iid)
 
@@ -89,65 +95,3 @@ test_that("Invalid header", {
 
 # 4. Data Structure Test
 
-# Equality Tests:
-#   expect_equal(): Checks if two objects are equal.
-# expect_identical(): Checks if two objects are identical (same object in memory).
-# expect_equivalent(): Checks if two objects are equivalent, accounting for differences in attributes or class.
-#
-# Comparison Tests:
-#   expect_true(): Checks if an expression is TRUE.
-# expect_false(): Checks if an expression is FALSE.
-# expect_less_than(): Checks if a value is less than another value.
-# expect_greater_than(): Checks if a value is greater than another value.
-# expect_match(): Checks if a character string matches a regular expression.
-#
-# Error Handling:
-#   expect_error(): Checks if an expression throws an error.
-# expect_warning(): Checks if an expression generates a warning.
-# expect_message(): Checks if an expression generates a specific message.
-#
-# Data Structure Tests:
-#   expect_length(): Checks the length of an object.
-# expect_is(): Checks if an object is of a specific class.
-# expect_inherits(): Checks if an object inherits from a specific class.
-# expect_named(): Checks if the elements of an object have specific names.
-#
-# Test Context:
-#   skip_if(): Skips a test if a certain condition is met.
-# skip(): Skips a test unconditionally.
-# context(): Defines a new context or grouping for related tests.
-
-# # In reality, our function is more complex and aggregates your input if you have duplicates in your id-time units -- this is why the following two tests were essential for us
-# ## Test whether the output contains the right number of rows
-# test_that("overview_tab() returns a dataframe with correct number of rows", {
-#   output_table <- overview_tab(dat = toydata, id = ccode, time = year)
-#   expect_equal(nrow(output_table), length(unique(toydata$ccode)))
-# })
-
-# ## Test whether the function works on a data frame that has no duplicates in id-time
-# test_that("overview_tab() works on a dataframe that is already in the correct
-#           format",
-#           {
-#             df_com <- data.frame(
-#               # Countries
-#               ccode  = c(
-#                 rep("RWA", 4),
-#                 rep("AGO", 8),
-#                 rep("BEN", 2),
-#                 rep("GBR", 5),
-#                 rep("FRA", 3)
-#               ),
-#               # Time frame
-#               year =
-#                 c(
-#                   seq(1990, 1995),
-#                   seq(1990, 1992),
-#                   seq(1995, 1999),
-#                   seq(1991, 1999, by = 2),
-#                   seq(1993, 1999, by = 3)
-#                 )
-#             )
-#             output_table <-
-#               overview_tab(dat = df_com, id = ccode, time = year)
-#             expect_equal(nrow(output_table), 5)
-#           })
